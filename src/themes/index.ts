9dcaf26be4c960b0822f11949acb586c3661e7dd
guiireal <guii_real@hotmail.com>
2025-11-05 22:49:19 -0300

    feat: add color tokens

---
+++ src/themes/index.ts
@@ -0,0 +1,28 @@
+export const theme = {
+  color: {
+    background: "#f2f2f2",
+    backgroundHighlight: "rgba(224, 213, 59, 0.17)",
+    error: "#ff0000",
+    highlight: "#e0d53b",
+    paper: "#ffffff",
+    primary: "#926dff",
+    primaryText: "#ffffff",
+    text: "#000000",
+    textPlaceholder: "#a1a1a1",
+  },
+  font: {
+    size: {
+      title: 24,
+      subtitle: 20,
+      body: 14,
+    },
+    familiy: {
+      regular: "",
+      bold: "",
+      italic: "",
+    },
+  },
+  shadow: {
+    default: "0 0 4 0 rgba(0, 0, 0, 0.25)",
+  },
+};
